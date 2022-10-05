2f29246670768be223ed5565f4f938f09406e2f8
William Kemper <kemperw@amazon.com>
2022-10-05 15:21:34 -0700

    Remove unused import

--- src/main/java/com/amazon/Wormhole.java
+++ src/main/java/com/amazon/Wormhole.java
@@ -7,7 +7,6 @@ import picocli.CommandLine;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
